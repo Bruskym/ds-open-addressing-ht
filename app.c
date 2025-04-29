0a34c4dcea11f0090d8c07a808e61d7bd0e9e008
Antonio Nascimento <ticoanaf@gmail.com>
2025-04-29 18:35:31 -0300

    feat: add example usage of hash table in app.c

--- app.c
+++ app.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 #include "ht_structure.h"
 
-
 int main(){
-    
+    hash_table* ht = new_ht();
+    insert(ht, "name", "Alice");
+    insert(ht, "job", "Engineer");
+
+    char* value = search(ht, "name");
+    printf("Name: %s\n", value);
+
+    delete(ht, "job");
+    delete_hash_table(ht);
 }
